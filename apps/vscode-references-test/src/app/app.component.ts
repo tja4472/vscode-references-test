0f1ed565298f5b62c8acbc566a0c0c13ed00cddb
Tim <tja2353@gmail.com>
2022-01-08 11:05:07 +0000

    initial commit

---
+++ apps/vscode-references-test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-workspace-vscode-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'vscode-references-test';
+}
